7f034d062786998eec6180aa991ad80c8e9c153f
sven <sven@gwelt.net>
2018-09-10 23:48:33 +0200

    delete/groom

--- index.js
+++ index.js
@@ -11,33 +11,40 @@ var db=new Group();
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
   db.load_from_file('data.json',(group)=>{console.log(JSON.stringify(group))});
-  db.addTeam({"id":"99","name":"Testteam","weekdays":[4,1],"time":"18:00"});
+  //db.addTeam({"id":"fbhh","name":"Fußball","recurrence":[{"weekday":4,"time":"18:30"}]});
 });
 
 app.use('/api/:r', function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin','*');
   switch (req.params.r) {
+    case 'addTeam':
+		  res.json(db.addTeam({"id":"fbhh","name":"Testteam","recurrence":[{"weekday":1,"time":"18:00"},{"weekday":3,"time":"20:00"}]}));
+      break;
     case 'getTeam':
-      res.json(db.getTeam('99'));
+      res.json(db.getTeam('fbhh'));
+      break;
+    case 'addEvent':
+      let ateam=db.findTeam('fbhh');
+      if (ateam) {res.json(ateam.addEvent({"datetime":"2018-10-13T11:11"}))} else {res.json(false)}
       break;
     case 'attend':
-      let aevent=db.findEvent('99','2018-10-01T18:00');
+      let aevent=db.findEvent('fbhh','2018-09-13T18:30');
       if (aevent) {res.json(aevent.attend('sven'))} else {res.json(false)}
       break;
     case 'refuse':
-      let revent=db.findEvent('99','2018-10-01T18:00');
+      let revent=db.findEvent('fbhh','2018-09-13T18:30');
       if (revent) {res.json(revent.refuse('sven'))} else {res.json(false)}
       break;
     case 'undecided':
-      let uevent=db.findEvent('99','2018-10-01T18:00');
+      let uevent=db.findEvent('fbhh','2018-09-13T18:30');
       if (uevent) {res.json(uevent.undecided('sven'))} else {res.json(false)}
       break;
-    case 'addEvent':
-      let team=db.findTeam('99');
-      if (team) {res.json(team.addEvent({"datetime":"2018-10-01T18:00"}))} else {res.json(false)}
+    case 'deleteEvent':
+      let dteam=db.findTeam('fbhh');
+      if (dteam) {res.json(dteam.deleteEvent({"datetime":"2018-10-13T11:11"}))} else {res.json(false)}
       break;
-    case 'addTeam':
-      res.json(db.addTeam({"id":"99","name":"Testteam","weekdays":[4],"time":"18:00"}));
+    case 'deleteTeam':
+		  res.json(db.deleteTeam({"id":"fbhh"}));
       break;
     /*
     case 'load':
@@ -57,24 +64,27 @@ app.use('/api/:r', function (req, res, next) {
       res.json(db);
       break;
     default:
-      res.send('Valid API-calls: <a href=/api/dump>/api/dump</a>');
+      res.send('no a valid API-call');
   }
 })
 app.use(express.static(path.join(__dirname, 'public')));
 
-function Group() {
-  this.teams=[];
-}
+function Group() {}
 
 function Team(json) {
   //todo: stealthen
   this.id=json.hasOwnProperty('id')?json.id:undefined;
   this.name=json.hasOwnProperty('name')?json.name:undefined;
-  this.weekdays=(json.weekdays instanceof Array)?json.weekdays:undefined;
-  this.time=json.hasOwnProperty('time')?json.time:undefined;
+  if (json.recurrence instanceof Array) {
+  	this.recurrence=[];
+  	json.recurrence.forEach((rc)=>{
+  		if ((rc.hasOwnProperty('weekday'))&&(rc.hasOwnProperty('time'))) {this.recurrence.push({"weekday":rc.weekday,"time":rc.time})}
+  	});
+  	if (!this.recurrence.length) {this.recurrence=undefined}
+  } else {this.recurrence=undefined}
   this.admintoken=json.hasOwnProperty('admintoken')?json.admintoken:undefined;
   this.teamtoken=json.hasOwnProperty('teamtoken')?json.teamtoken:undefined;
-  if (typeof this.id !== 'undefined') {this.events=[]};
+  //todo: use of teamtoken
 }
 
 function Event(json) {
@@ -87,6 +97,7 @@ function Event(json) {
 }
 
 Group.prototype.load_from_file = function(filename,callback) {
+  //todo: use of sysop-token
   this.teams=[];
   fs.readFile(filename, 'utf8', (err, data)=>{
     if (err){console.log('No data-file.')} else {
@@ -105,19 +116,28 @@ Group.prototype.load_from_file = function(filename,callback) {
 }
 
 Group.prototype.save_to_file = function(filename,callback) {
+  //todo: use of sysop-token
   fs.writeFile(filename, JSON.stringify(this), 'utf8', (err)=>{console.log('File saved. Errors: '+err);callback(this)});
 }
 
 Group.prototype.findTeam = function(teamid) {
-  return this.teams.find(t => t.id==teamid)||false;
+  //todo: use of teamtoken
+  if (this.teams instanceof Array) {
+    return this.teams.find(t => t.id==teamid)||false;
+  } else {return false}
 }
 
 Group.prototype.getTeam = function(teamid) {
+  //todo: use of teamtoken
   let t=this.findTeam(teamid);
   if (t) {
     // groom: delete all past events
-    t.events=t.events.filter(e=>e.datetime>(getDateString()));
-    // backfill: generate missing future events, if any weekday for recurring events is specified in t.weekdays
+    let d=getDateString();
+    t.events=t.events.filter(e=>e.datetime>d);
+    // groom: delete all events that have no data yet (=events that only have one property that has to be 'datetime')
+    // this will delete all automatic generated events without changes/edits (assumption is, that manually added events will have at least one other property)
+    t.events=t.events.filter(e=>Object.values(e).reduce((a,c)=>(c!==undefined)?a+1:a,0)>1);
+    // backfill: generate future events, if any recurrance is specified
     t.generateNextRecurringEvents();
     // order events by date
     t.events.sort((a,b)=>{return a.datetime>b.datetime});
@@ -132,9 +152,9 @@ function getDateString(d) {
 } 
 
 Team.prototype.generateNextRecurringEvents = function(count) {
-  if (this.weekdays instanceof Array) {
-    this.weekdays.forEach((wd)=>{
-      getNextDaysWithWeekday(wd,count).forEach((d)=>{this.addEvent({"datetime":d+'T'+(this.time||'00:00')})});
+  if (this.recurrence instanceof Array) {
+    this.recurrence.forEach((rc)=>{
+      getNextDaysWithWeekday(rc.weekday,count).forEach((d)=>{this.addEvent({"datetime":d+'T'+(rc.time||'00:00')})});
     });
   }
 }
@@ -151,44 +171,65 @@ function getNextDaysWithWeekday(weekday,count) {
 
 Group.prototype.addTeam = function(json) {
   let team=new Team(json);
-  if ( (this.hasOwnProperty('teams'))
-     &&(this.teams instanceof Array)
-     &&(typeof team.id !== 'undefined')
-     &&(!this.findTeam(team.id))
-     )
+  if (!(this.teams instanceof Array)) {this.teams=[]};
+  if ( (typeof team.id !== 'undefined') && (!this.findTeam(team.id)) )
   {
     this.teams.push(team);
     return team;
   } else {
+ 		if (!this.teams.length) {this.teams=undefined};
     return false;
   }
 }
 
+Group.prototype.deleteTeam = function(json) {
+  //todo: use of admintoken
+	if ( (json.hasOwnProperty('id')) && (this.findTeam(json.id)) ) {
+		this.teams=this.teams.filter(t=>t.id!==json.id);
+ 		if (!this.teams.length) {this.teams=undefined; return [];};
+		return this.teams;
+	} else {return false}
+}
+
 Group.prototype.findEvent = function(teamid,datetime) {
+  //todo: use of teamtoken
   let team=this.findTeam(teamid);
   if (team) {return team.findEvent(datetime)} else {return false}
 }
 
 Team.prototype.findEvent = function(datetime) {
-  return this.events.find(e => e.datetime==datetime)||false;
+  //todo: use of teamtoken
+  if (this.events instanceof Array) {
+  	return this.events.find(e => e.datetime==datetime)||false;
+  } else {return false}
 }
 
 Team.prototype.addEvent = function(json) {
+  //todo: use of admintoken
   let event=new Event(json);
-  if ( (this.hasOwnProperty('events'))
-     &&(this.events instanceof Array)
-     &&(typeof event.datetime !== 'undefined')
-     &&(!this.findEvent(event.datetime))
-     )
+  if (!(this.events instanceof Array)) {this.events=[]};
+  if ( (typeof event.datetime !== 'undefined') && (!this.findEvent(event.datetime)) )
   {
     this.events.push(event);
     return event;
   } else {
+ 		if (!this.events.length) {this.events=undefined};
     return false;
   }
 }
 
+Team.prototype.deleteEvent = function(json) {
+  //todo: use of admintoken
+	if ( (json.hasOwnProperty('datetime')) && (this.findEvent(json.datetime)) ) {
+		this.events=this.events.filter(e=>e.datetime!==json.datetime);
+ 		if (!this.events.length) {this.events=undefined; return [];};
+ 		return this.events;
+	} else {return false}
+}
+
 Event.prototype.attend = function(name) {
+  //todo: use of teamtoken
+  //todo: stealthen
   if (!(this.attendees instanceof Array)) {this.attendees=[]};
   if (!this.attendees.includes(name)) {this.attendees.push(name)};
   if (this.refusals instanceof Array) {this.refusals=this.refusals.filter(r=>r!==name); if (!this.refusals.length) {this.refusals=undefined};};
@@ -196,6 +237,8 @@ Event.prototype.attend = function(name) {
 }
 
 Event.prototype.refuse = function(name) {
+  //todo: use of teamtoken
+  //todo: stealthen
   if (!(this.refusals instanceof Array)) {this.refusals=[]};
   if (!this.refusals.includes(name)) {this.refusals.push(name)};
   if (this.attendees instanceof Array) {this.attendees=this.attendees.filter(a=>a!==name); if (!this.attendees.length) {this.attendees=undefined};};
@@ -203,6 +246,8 @@ Event.prototype.refuse = function(name) {
 }
 
 Event.prototype.undecided = function(name) {
+  //todo: use of teamtoken
+  //todo: stealthen
   if (this.attendees instanceof Array) {this.attendees=this.attendees.filter(a=>a!==name); if (!this.attendees.length) {this.attendees=undefined};};
   if (this.refusals instanceof Array) {this.refusals=this.refusals.filter(r=>r!==name); if (!this.refusals.length) {this.refusals=undefined};};
   return this;
