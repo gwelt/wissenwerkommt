fb9e20072c0b13f68ce6a6ab56cbbaf8b74a3bff
sven <sven@gwelt.net>
2018-10-03 21:37:15 +0200

    REST-API-tests with supertest (includes mocha)

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ var port = process.env.PORT || config.port || 3000;
 var Group = require('./group.js');
 var db=new Group();
 server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-  db.load_from_file(config.datafilepath+'/'+config.datafile,(group)=>{console.log(JSON.stringify(group))});
+  //console.log('Server listening at port %d', port);
+  db.load_from_file(config.datafilepath+'/'+config.datafile,(group)=>{/*console.log(JSON.stringify(group))*/});
 });
+module.exports = app;
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/:r/:t?', function (req, res) {
@@ -36,88 +36,88 @@ app.use('/api/:r/:t?', function (req, res) {
     case 'getTeam':
       if (getUserLevel(req)>-1) {
         res.json(db.getTeam(req.params.t||req.body.teamid));
-      } else {res.json({'error':'not sufficient rights to get team'})}
+      } else {res.status(401).json({'error':'not sufficient rights to get team'})}
       break;
     case 'attend':
       if (getUserLevel(req)>0) {
         let aevent=db.findEvent(req.body.teamid,req.body.datetime);
-        if (aevent) {res.json(aevent.attend(req.body.name))} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to attend'})}
+        if (aevent) {res.json(aevent.attend(req.body.name))} else {res.status(403).json({'error':'event not found'})}
+      } else {res.status(401).son({'error':'not sufficient rights to attend'})}
       break;
     case 'refuse':
       if (getUserLevel(req)>0) {
         let revent=db.findEvent(req.body.teamid,req.body.datetime);
-        if (revent) {res.json(revent.refuse(req.body.name))} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to refuse'})}
+        if (revent) {res.json(revent.refuse(req.body.name))} else {res.status(403).json({'error':'event not found'})}
+      } else {res.status(401).json({'error':'not sufficient rights to refuse'})}
       break;
     case 'undecided':
       if (getUserLevel(req)>0) {
         let uevent=db.findEvent(req.body.teamid,req.body.datetime);
-        if (uevent) {res.json(uevent.undecided(req.body.name))} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to undecide'})}
+        if (uevent) {res.json(uevent.undecided(req.body.name))} else {res.status(403).json({'error':'event not found'})}
+      } else {res.status(401).json({'error':'not sufficient rights to undecide'})}
       break;
     case 'commentEvent':
       if (getUserLevel(req)>0) {
         let cevent=db.findEvent(req.body.teamid,req.body.datetime);
-        if (cevent) {res.json(cevent.commentEvent(req.body.comment))} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to comment event'})}
+        if (cevent) {res.json(cevent.commentEvent(req.body.comment))} else {res.status(403).json({'error':'event not found'})}
+      } else {res.status(401).json({'error':'not sufficient rights to comment event'})}
       break;
   
     // team admins only
     case 'editTeam':
         if (getUserLevel(req)>1) {
         res.json(db.editTeam(req.body));
-      } else {res.json({'error':'not sufficient rights to edit team'})}
+      } else {res.status(401).json({'error':'not sufficient rights to edit team'})}
       break;
     case 'deleteTeam':
         if (getUserLevel(req)>1) {
         res.json(db.deleteTeam(req.body));
-      } else {res.json({'error':'not sufficient rights to delete team'})}
+      } else {res.status(401).json({'error':'not sufficient rights to delete team'})}
       break;
     case 'addEvent':
       if (getUserLevel(req)>1) {
         let ateam=db.findTeam(req.body.teamid);
         if (ateam) {res.json(ateam.addEvent({"datetime":req.body.datetime,"comment":req.body.comment}))} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to add event'})}
+      } else {res.status(401).json({'error':'not sufficient rights to add event'})}
       break;
     case 'cancelEvent':
       if (getUserLevel(req)>1) {
         let devent=db.findEvent(req.body.teamid,req.body.datetime);
         if (devent) {res.json(devent.cancelEvent())} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to cancel event'})}
+      } else {res.status(401).json({'error':'not sufficient rights to cancel event'})}
       break;
     case 'reviveEvent':
       if (getUserLevel(req)>1) {
         let vevent=db.findEvent(req.body.teamid,req.body.datetime);
         if (vevent) {res.json(vevent.reviveEvent())} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to revive event'})}
+      } else {res.status(401).json({'error':'not sufficient rights to revive event'})}
       break;
     case 'deleteEvent':
       if (getUserLevel(req)>1) {
         let dteam=db.findTeam(req.body.teamid);
         if (dteam) {res.json(dteam.deleteEvent({"datetime":req.body.datetime}))} else {res.json(false)}
-      } else {res.json({'error':'not sufficient rights to delete event'})}
+      } else {res.status(401).json({'error':'not sufficient rights to delete event'})}
       break;
 
     // sysops only
     case 'load':
       if (getUserLevel(req)>2) {
         db.load_from_file(config.datafilepath+'/'+config.datafile,(db)=>{res.json(db)});
-      } else {res.json({'error':'not sufficient rights to do that (load)'})}
+      } else {res.status(401).json({'error':'not sufficient rights to do that (load)'})}
       break;
     case 'save':
       if (getUserLevel(req)>2) {
         db.save_to_file(config.datafilepath+'/'+config.datafile,(db)=>{res.json(db)});
-      } else {res.json({'error':'not sufficient rights to do that (save)'})}
+      } else {res.status(401).json({'error':'not sufficient rights to do that (save)'})}
       break;
     case 'dump':
       if (getUserLevel(req)>2) {
         res.json(db);
-      } else {res.json({'error':'not sufficient rights to do that (dump)'})}
+      } else {res.status(401).json({'error':'not sufficient rights to do that (dump)'})}
       break;
 
     default:
-      res.json({'error':'not a valid AJAX-API-call'});
+      res.status(400).json({'error':'not a valid AJAX-API-call'});
 
   }
 
@@ -126,12 +126,6 @@ app.use('/api/:r/:t?', function (req, res) {
 app.use(function(req, res, next){
   res.sendFile('index.html', { root: path.join(__dirname, 'public')});
 });
-
-function getDateString(d) {
-  if (!d) {d=new Date()};
-  var tzoffset = d.getTimezoneOffset() * 60000;
-  return (new Date(d-tzoffset)).toISOString().slice(0, -14);
-} 
 
 function getUserLevel(req) {
   let team=db.findTeam(req.params.t||req.body.teamid);
@@ -150,6 +144,12 @@ function getUserLevel(req) {
   //console.log('team:'+req.body.teamid+' teamtoken:'+team.teamtoken+' usertoken:'+req.body.token+' = '+userLevel);
   return userLevel;
 };
+
+function getDateString(d) {
+  if (!d) {d=new Date()};
+  var tzoffset = d.getTimezoneOffset() * 60000;
+  return (new Date(d-tzoffset)).toISOString().slice(0, -14);
+} 
 
 process.on('SIGINT', function(){console.log('SIGINT'); db.save_to_file(config.datafilepath+'/temp_'+getDateString().slice(8,10)+'_'+config.datafile,()=>{process.exit()}); });
 process.on('SIGTERM', function(){console.log('SIGTERM'); db.save_to_file(config.datafilepath+'/temp_'+getDateString().slice(8,10)+'_'+config.datafile,()=>{process.exit()}); });
--- test/test.js
+++ test/test.js
@@ -1,27 +1,225 @@
-var assert = require('chai').assert;
-var Group = require('../group.js');
-var db=new Group();
+/* SUPERTEST HTTP/REST-API TESTS */
+const request = require('supertest');
+var app = require('../index');
 
-describe('Group functions', function() {
-	describe('getListOfTeamIDs', function () {
-		it('should return the list of current teams', function () {
-			assert.isArray(db.getListOfTeamIDs(),'isArray');
-			assert.deepEqual(db.getListOfTeamIDs(),[]);
-		});
-	});
-	describe('addTeam', function () {
-		it('should add a team to the list of teams, if the team is not present', function () {
-			db.addTeam({"teamid":"team1"});
-			db.addTeam({"teamid":"team1"});
-			assert.include(db.getListOfTeamIDs(),'team1');
-			assert.lengthOf(db.getListOfTeamIDs(),1);
-			db.addTeam({"teamid":"team2"});
-			assert.include(db.getListOfTeamIDs(),'team2');
-			assert.lengthOf(db.getListOfTeamIDs(),2);
-		});
-	});
+// OPEN REST-REQUESTS
+
+describe('POST /api/addTeam', function() {
+  it('should add Team G20', function(done) {
+    request(app)
+      .post('/api/addTeam')
+      .send({teamid:'teamG20',name:'Team G20',recurrence:[{weekday:"7",time:"12:15"}]})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
 });
 
-/*
-db.load_from_file('../data/data.json',(d)=>{console.log(d)});
-*/
+describe('GET /api/getListOfTeamIDs', function() {
+  it('should respond with a list of all team-IDs', function(done) {
+    request(app)
+      .get('/api/getListOfTeamIDs')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+	  .end(function(err, res) {
+	    if (err) throw err;
+	    console.log(res.text);
+	    done();
+	  });
+  });
+});
+
+describe('GET /api/stats', function() {
+  it('respond with json', function(done) {
+    request(app)
+      .get('/api/stats')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+	  .end(function(err, res) {
+	    if (err) throw err;
+	    console.log(res.text);
+	    done();
+	  });
+  });
+});
+
+// TEAM REST-REQUESTS
+
+describe('GET /api/getTeam', function() {
+  it('should get /api/getTeam of teamG20', function(done) {
+    request(app)
+      .get('/api/getTeam')
+      .send({teamid:'teamG20'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+function getDateString(d) {
+  if (!d) {d=new Date()};
+  var tzoffset = d.getTimezoneOffset() * 60000;
+  return (new Date(d-tzoffset)).toISOString().slice(0, -14);
+} 
+
+describe('PUT /api/attend a non-existing event', function() {
+  it('should deny attending an event with HTTP status code 403', function(done) {
+    request(app)
+      .put('/api/attend')
+      .send({teamid:'teamG20',datetime:'2011-10-06T12:15',name:'SuperSven'})
+      .set('Accept', 'application/json')
+      .expect(403)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('PUT /api/attend', function() {
+  it('should attend an event', function(done) {
+    request(app)
+      .put('/api/attend')
+      .send({teamid:'teamG20',datetime:getDateString()+'T12:15',name:'SuperSven'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('PUT /api/commentEvent', function() {
+  it('should comment an event', function(done) {
+    request(app)
+      .put('/api/commentEvent')
+      .send({teamid:'teamG20',datetime:getDateString()+'T12:15',comment:'I like this event.'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('GET /api/getTeam', function() {
+  it('should get /api/getTeam of teamG20', function(done) {
+    request(app)
+      .get('/api/getTeam')
+      .send({teamid:'teamG20'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('PUT /api/refuse', function() {
+  it('should refuse an event', function(done) {
+    request(app)
+      .put('/api/refuse')
+      .send({teamid:'teamG20',datetime:getDateString()+'T12:15',name:'SuperSven'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('PUT /api/undecided', function() {
+  it('should undecide an event', function(done) {
+    request(app)
+      .put('/api/undecided')
+      .send({teamid:'teamG20',datetime:getDateString()+'T12:15',name:'SuperSven'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('PUT /api/commentEvent', function() {
+  it('should comment an event', function(done) {
+    request(app)
+      .put('/api/commentEvent')
+      .send({teamid:'teamG20',datetime:getDateString()+'T12:15',comment:''})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('GET /api/getTeam', function() {
+  it('should get /api/getTeam of teamG20', function(done) {
+    request(app)
+      .get('/api/getTeam')
+      .send({teamid:'teamG20'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+// TEAM-ADMIN REST-REQUESTS
+
+describe('DELETE /api/deleteTeam', function() {
+  it('should delete teamG20', function(done) {
+    request(app)
+      .delete('/api/deleteTeam')
+      .send({teamid:'teamG20'})
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) throw err;
+        console.log(res.text);
+        done();
+      });
+  });
+});
+
+describe('GET /api/getListOfTeamIDs', function() {
+  it('should respond with a list of all team-IDs', function(done) {
+    request(app)
+      .get('/api/getListOfTeamIDs')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+	  .end(function(err, res) {
+	    if (err) throw err;
+	    console.log(res.text);
+	    done();
+	  });
+  });
+});
