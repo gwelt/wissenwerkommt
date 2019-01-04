24a348ad3fa17061b7db37c0d6bbde1d3a79acf3
sven <sven@gwelt.net>
2019-01-04 21:23:28 +0100

    mark cancellation, memory stats

--- group.js
+++ group.js
@@ -234,7 +234,7 @@ Group.prototype.stats = function() {
   eventCount=this.teams.reduce((a,t)=>a+=t.events?t.events.length:0,0);
   attendCount=this.teams.reduce((a,t)=>a+=t.events?t.events.reduce((b,u)=>b+=u.attendees?u.attendees.length:0,0):0,0);
   refusalCount=this.teams.reduce((a,t)=>a+=t.events?t.events.reduce((b,u)=>b+=u.refusals?u.refusals.length:0,0):0,0);
-  return {"teams":teamCount,"events":eventCount,"attendees":attendCount,"refusals":refusalCount};
+  return {"teams":teamCount,"events":eventCount,"attendees":attendCount,"refusals":refusalCount,"memorysize":memorySizeOf(this),"JSONsize":formatByteSize(JSON.stringify(this).length)};
 }
 
 Group.prototype.load_from_file = function(filename,callback) {
@@ -279,6 +279,42 @@ Team.prototype.findEvent = function(datetime) {
     return this.events.find(e => e.datetime==datetime)||false;
   } else {return false}
 }
+
+function memorySizeOf(obj) {
+    var bytes = 0;
+    function sizeOf(obj) {
+        if(obj !== null && obj !== undefined) {
+            switch(typeof obj) {
+            case 'number':
+                bytes += 8;
+                break;
+            case 'string':
+                bytes += obj.length * 2;
+                break;
+            case 'boolean':
+                bytes += 4;
+                break;
+            case 'object':
+                var objClass = Object.prototype.toString.call(obj).slice(8, -1);
+                if(objClass === 'Object' || objClass === 'Array') {
+                    for(var key in obj) {
+                        if(!obj.hasOwnProperty(key)) continue;
+                        sizeOf(obj[key]);
+                    }
+                } else bytes += obj.toString().length * 2;
+                break;
+            }
+        }
+        return bytes;
+    };
+    return formatByteSize(sizeOf(obj));
+};
+function formatByteSize(bytes) {
+    if(bytes < 1024) return bytes + " bytes";
+    else if(bytes < 1048576) return(bytes / 1024).toFixed(3) + " KiB";
+    else if(bytes < 1073741824) return(bytes / 1048576).toFixed(3) + " MiB";
+    else return(bytes / 1073741824).toFixed(3) + " GiB";
+};
 
 const crypto = require('crypto');
 function crypt(str) {
